a387cc2a74042e88d8ac135ebed2037874b91247
Tharre <tharre3@gmail.com>
2014-05-08 15:03:35 +0200

    Formating changes and compatibility macros

--- murmur3.h
+++ murmur3.h
@@ -1,14 +1,15 @@
-//-----------------------------------------------------------------------------
-// MurmurHash3 was written by Austin Appleby, and is placed in the
-// public domain. The author hereby disclaims copyright to this source
-// code.
+/*
+ * MurmurHash3 was written by Austin Appleby, and is placed in the
+ * public domain. The author hereby disclaims copyright to this source
+ * code.
+ */
 
 #ifndef _MURMURHASH3_H_
 #define _MURMURHASH3_H_
 
 #include <stdint.h>
 
-//-----------------------------------------------------------------------------
+/*----------------------------------------------------------------------------*/
 
 void MurmurHash3_x86_32 (const void *key, int len, uint32_t seed, void *out);
 
@@ -16,6 +17,6 @@ void MurmurHash3_x86_128(const void *key, int len, uint32_t seed, void *out);
 
 void MurmurHash3_x64_128(const void *key, int len, uint32_t seed, void *out);
 
-//-----------------------------------------------------------------------------
+/*----------------------------------------------------------------------------*/
 
-#endif // _MURMURHASH3_H_
+#endif /* _MURMURHASH3_H_ */
